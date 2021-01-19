f523359fe2944ab30c711b84c670750a61a4423f
Kloska <56342396+Kloska@users.noreply.github.com>
2021-01-19 10:28:26 +0100

    Randomizer med hjälp från Magnus

--- src/Randomizer.java
+++ src/Randomizer.java
@@ -2,7 +2,13 @@ import java.util.Random;
 
 public class Randomizer {
     public static void main(String[] args) {
-
+    String word = RandomWord();
     }
+
+    private static String RandomWord() {
+        String[] words = {"Nagel","betyg","debut","fakta"};
+        Random R = new Random();
+        int randomNumber = R.nextInt(words.length);
+        return words[randomNumber];
     }
 }

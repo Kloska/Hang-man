93c6cd4c6e9791e284d3d036e5f0fc5fe59e6a09
Kloska <56342396+Kloska@users.noreply.github.com>
2021-01-19 11:10:44 +0100

    Scanner för att läsa in gissningen

--- src/LetterGuess.java
+++ src/LetterGuess.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class LetterGuess {
+    public static void main(String[] args) {
+        Scanner ReadGuess = new Scanner(System.in);
+        System.out.println("Gissa på en bokstav");
+        String Guess = ReadGuess.nextLine();
+        System.out.println("Du gissade på" + Guess);
+    }
 }

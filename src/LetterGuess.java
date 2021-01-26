17a97ff8af2f187dea30f279dfa7866b178476a1
Kloska <56342396+Kloska@users.noreply.github.com>
2021-01-26 10:55:04 +0100

    Input
    Gjorde all input till uppercase

--- src/LetterGuess.java
+++ src/LetterGuess.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 
+import static jdk.nashorn.internal.objects.NativeString.toUpperCase;
+
 public class LetterGuess {
     public static void main(String[] args) {
         Scanner ReadGuess = new Scanner(System.in);
         System.out.println("Gissa på en bokstav");
         String Guess = ReadGuess.nextLine();
-        System.out.println("Du gissade på" + Guess);
+        System.out.println("Du gissade på " + toUpperCase(Guess) + ".");
     }
 }

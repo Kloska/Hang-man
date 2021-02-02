8dc84e64fb5457b3b8e88b203b42e5773e716d3a
Kloska <56342396+Kloska@users.noreply.github.com>
2021-02-02 11:09:34 +0100

    Omstrukturering

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,7 +1,10 @@
+import javax.swing.*;
+
 public class Hangman {
     public static void main(String[] args) {
         String word = Randomizer.RandomWord();
         String guess = LetterGuess.Guess();
     }
+    
 
 }
--- src/Randomizer.java
+++ src/Randomizer.java
@@ -1,4 +1,6 @@
 import java.util.Random;
+import java.util.Scanner;
+import static jdk.nashorn.internal.objects.NativeString.toUpperCase;
 
 public class Randomizer {
     public static void main(String[] args) {
@@ -6,9 +8,19 @@ public class Randomizer {
     }
 
     public static String RandomWord() {
-        String[] words = {"Nagel","betyg","debut","fakta"};
+        String[] words = {"nagel","betyg","debut","fakta"};
         Random R = new Random();
         int randomNumber = R.nextInt(words.length);
         return words[randomNumber];
+    }
+}
+
+class LetterGuess {
+    public static String Guess() {
+        Scanner ReadGuess = new Scanner(System.in);
+        System.out.println("Gissa på en bokstav");
+        String Guess = ReadGuess.nextLine();
+        System.out.println("Du gissade på " + toUpperCase(Guess) + ".");
+        return Guess;
     }
 }
